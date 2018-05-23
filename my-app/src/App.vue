605d5449158953d9cf190e827cd688d33a718a05
Kunimi Nagakura <kunimin@linecorp.com>
2018-05-23 13:40:18 +0900

    feat : change icon

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -13,7 +13,7 @@
           <router-link to="/">
             <v-list-tile>
               <v-list-tile-action>
-                <v-icon>edit</v-icon>
+                <v-icon>person</v-icon>
               </v-list-tile-action>
               <v-list-tile-content>
                 <v-list-tile-title>Dashboard</v-list-tile-title>
